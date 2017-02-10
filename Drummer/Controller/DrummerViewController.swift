c8a25b09338098fdd43f175b572ef21e09eff77f
Wilson Wang <wilsonw926@gmail.com>
2017-02-09 19:58:49 -0800

    drummer

--- Drummer/Controller/DrummerViewController.swift
+++ Drummer/Controller/DrummerViewController.swift
@@ -27,5 +27,8 @@ class DrummerViewController: UIViewController {
     
     // YOUR CODE HERE
 
+    @IBAction func drumButtonWasPressed(_ sender: UIButton) {
+        currentDrumKit?.playDrumSound(forDrumWithTag: sender.tag)
+    }
 
 }
